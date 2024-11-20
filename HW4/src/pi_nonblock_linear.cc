e80f96d73ea14dcaada8bf4ff2b602dae66fa700
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-11-21 03:11:47 +0800

    mpi request

--- HW4/src/pi_nonblock_linear.cc
+++ HW4/src/pi_nonblock_linear.cc
@@ -45,8 +45,8 @@ int main(int argc, char **argv)
     {
         // TODO: non-blocking MPI communication.
         // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
-        MPI_Request* requests;
-        requests = (MPI_Request*)malloc((world_size - 1) * sizeof(MPI_Request));
+        MPI_Request requests[world_size - 1];
+        // requests = (MPI_Request*)malloc((world_size - 1) * sizeof(MPI_Request));
         long long int *local_count = (long long int *)malloc((world_size - 1) * sizeof(long long int));
 
         for (int i = 1; i < world_size; i++) {
