690ae2c93a1cc2d1309d1c57cb08805d514886db
stanleyshen2003 <stanley.shen2003@gmail.com>
2024-11-21 04:02:41 +0800

    mpi request

--- HW4/src/pi_one_side.cc
+++ HW4/src/pi_one_side.cc
@@ -48,7 +48,9 @@ int main(int argc, char **argv)
             counts[i] = -1;
         counts[0] = count;
         MPI_Alloc_mem(world_size * sizeof(long long int), MPI_INFO_NULL, &counts);
-
+        for (int i = 0; i < world_size; i++)
+            counts[i] = -1;
+        counts[0] = count;
         MPI_Win_create(counts, world_size * sizeof(long long int), sizeof(long long int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
 
         bool ready = true;
